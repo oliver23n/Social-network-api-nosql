7ac445928fcacb94d327fbaadab520e2bc3b0ab8
oliver23n <oliver.nasevski@gmail.com>
2023-08-10 14:12:59 -0400

    finished models

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,37 @@
-const mongoose = require('mongoose');
+const { Schema, Types, model } = require('mongoose');
+const Reaction = require('./Reaction')
 
+const thoughtSchema = new Schema({
+
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength:1,
+        maxLength:280
+    },
+    username: {
+        type: String,
+        required: true,
+
+    },
+    reactions: [Reaction],
+    createdAt: {
+        type: Date,
+        default: () => Date.now()
+    }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function (){
+    return this.reactions.length
+})
+
+const Thought = model('Thought',thoughtSchema)
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,4 +1,43 @@
 const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+    
+    username :{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+
+    },
+    thoughts:[
+        {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: "Thought"
+        }
+    ],
+    friends: [{
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: "User"
+    }
+  ]
+},
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+const User  = mongoose.model('User',userSchema);
 
 module.exports = User;
