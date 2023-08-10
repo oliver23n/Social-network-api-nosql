c4c3a57211b8c3b5feb02173d034a70425afc738
oliver23n <oliver.nasevski@gmail.com>
2023-08-10 17:25:49 -0400

    fixed seed

--- utils/seed.js
+++ utils/seed.js
@@ -13,20 +13,21 @@ connection.once('open', async () => {
     await Thought.deleteMany({});
     // Create the parent collections (User)
     const users = names.map(name => ({ username: name, email: `${name.toLowerCase()}@gmail.com`, thoughts: [] }));
-    await User.collection.insertMany(users);
     // If you have related data, iterate over the data (thoughts) and for each, generate a (random number * the length of the user array)
     for (let i = 0; i < possibleThoughts.length; i++){
         const index = Math.floor(Math.random() * users.length);
         // Find the user at that index
         const randomUser = users[index];
-
+        
         // Insert that user's _id OR username into that thought doc
         const thought = new Thought({ thoughtText: possibleThoughts[i], username: randomUser.username });
         await thought.save();
         // Find and update the user whose _id or username was just inserted as the creator of the thought
         randomUser.thoughts.push(thought._id);
 
-     }
+        
+    }
+    await User.collection.insertMany(users);
     console.table(users);
     console.info('Seeding complete! 🌱');
     process.exit(0);
